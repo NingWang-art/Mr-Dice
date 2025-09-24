a55a61cc913a729d240e1f5e00bf8890ea741db4
NingWang <84500213+NingWang-art@users.noreply.github.com>
2025-09-24 14:44:41 +0800

    feat: Mr-Dice_v0.81 modify start script

--- start_all.sh
+++ start_all.sh
@@ -4,12 +4,18 @@ cd /home/Mr_Dice/bohriumpublic_database/Bohriumpublic_Server
 source bohrium_setup_env.sh
 source export_test_env.sh
 nohup python server.py 2>&1 &
+echo "Bohrium public server started"
 
 cd /home/Mr_Dice/mofdb_database/Mofdb_Server
 nohup python server.py 2>&1 &
+echo "MOFdb server started"
 
 cd /home/Mr_Dice/openlam_database/Openlam_Server
 nohup python server.py 2>&1 &
+echo "OpenLAM server started"
 
 cd /home/Mr_Dice/optimade_database/Optimade_Server
 nohup python server.py 2>&1 &
+echo "Optimade server started"
+
+ps -ef | grep "python"
